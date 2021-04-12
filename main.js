3efd6d78b8a0022684c8e1e40cdff1ae141f301e
dhildeb <d.hildebrandt93@gmail.com>
2021-04-12 15:13:07 -0600

    commit final

--- main.js
+++ main.js
@@ -1,73 +1,92 @@
 let contacts = []
 
-/**
- * Called when submitting the new Contact Form
- * This method will pull data from the form
- * use the provided function to give the data an id
- * then add that data to the contacts list.
- * Then reset the form
- * *** hints:
- * *** push: resources/push.jpg
- */
 function addContact(event) {
+  event.preventDefault()
+
+  let form = event.target
+  let contactName = form.contactName.value
+  let contactPhone = form.phone.value
+  let emergencyContact = document.getElementById("checkbox")
+
+  emergencyContact = emergencyContact.checked
   
+  contactInfo = {contactName, contactPhone, emergencyContact}
+
+  contacts.push(contactInfo)
+  saveContacts()
+  
+  form.reset()
 }
 
-/**
- * Converts the contacts array to a JSON string then
- * Saves the string to localstorage at the key contacts 
- */
 function saveContacts() {
- 
+  window.localStorage.setItem("contacts", JSON.stringify(contacts))
+  drawContacts()
 }
 
-/**
- * Attempts to retrieve the contacts string from localstorage
- * then parses the JSON string into an array. Finally sets
- * the contacts array to the retrieved array
- */
 function loadContacts() {
-  
+  let contactsData = JSON.parse(window.localStorage.getItem("contacts"))
+  if(contactsData){
+    contacts = contactsData
+}
 }
 
-/**
- * This function targets the contacts-list on the 
- * DOM and adds a new div element for each of the
- * contacts in the contacts array
- */
 function drawContacts() {
- 
+  let template = ""
+  
+    contacts.forEach(contact => {
+      
+      if(contact.emergencyContact == false){
+        template += 
+      `
+      <div class="card mt-1 mb-1">
+      <h3 class="mt-1 mb-1">
+      ${contact.contactName}
+      </h3>
+      <div class="d-flex space-between">
+        <p>
+          <i class="fa fa-fw fa-phone"></i>
+          <span>
+          ${contact.contactPhone}
+          </span>
+        </p>
+        <i class="action fa fa-trash text-danger" onclick="removeContact(${contacts.indexOf(contact)})"></i>
+      </div>
+    </div>
+    `}
+    else{
+      template += 
+    `
+    <div class="card mt-1 mb-1 emergency-contact">
+      <h3 class="mt-1 mb-1">${contact.contactName}
+      </h3>
+      <div class="d-flex space-between">
+        <p>
+          <i class="fa fa-fw fa-phone"></i>
+          <span>
+          ${contact.contactPhone}
+          </span>
+        </p>
+        <i class="action fa fa-trash text-danger" onclick="removeContact(${contacts.indexOf(contact)})"> </i>
+      </div>
+    </div>`
+    }
+    })
+    document.getElementById("contact-list").innerHTML = template
 }
 
-/**
- * This function is called with a contact id
- * and will use the id to find and remove the 
- * contact by their id from the list of contacts
- * *** hints: 
- * *** findIndex: resources/findIndex.jpg
- * *** splice: resources/splice.jpg
- * @param {string} contactId 
- */
 function removeContact(contactId) {
+let remove = contacts.splice(contactId,1)
+
+saveContacts()
 }
 
-/**
- * Toggles the visibility of the AddContact Form
- */
-function toggleAddContactForm() {
-
+function showContactForm(){
+  document.getElementById("new-contact-form").classList.remove("hidden")
 }
 
-
-/**
- * Used to generate a random string id for mocked
- * database generated Id
- * @returns {string}
- */
-function generateId() {
-  return Math.floor(Math.random() * 10000000) + "-" + Math.floor(Math.random() * 10000000)
+function hideContactForm(){
+  document.getElementById("new-contact-form").classList.add("hidden")
 }
-
 
 loadContacts()
 drawContacts()
